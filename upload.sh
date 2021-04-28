321f3163fbdad562e65c8481e4ac30babfc8feb4
epagnin <elena.pagnin@eit.lth.se>
2021-04-28 13:37:41 +0200

    updates

---
+++ upload.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+SOURCE="/Users/makoto/BlueCloud/mywebsite"
+DESTINATION="/Users/makoto/Papers/epagnin"
+
+bundle exec jekyll build
+
+cp -r "$SOURCE/_site/"* "$DESTINATION/"
+
+cd "$DESTINATION/"
+
+git add *
+
+git commit -m "updates"
+
+sudo git push 
+
+#USERNAME=elenap
+#RHOST=git@github.com:
+#RPATH=epagnin/epagnin.github.io.git/
+
+#bundle exec jekyll build
+
+#rsync -priv --delete --checksum $* \
+#  _site/ \
+#  ${RHOST}:${RPATH}
+
+# rm -r _site
